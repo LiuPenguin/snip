7ded0de7dad7d4d3ad386440e08b6a39951107c0
penguin <penguin_liu@163.com>
2019-11-17 12:03:33 +0800

    提交新的FFF

---
+++ snip/FFFFFFFF.h
@@ -0,0 +1,17 @@
+//
+//  FFFFFFFF.h
+//  snip
+//
+//  Created by YDKJ on 2019/11/17.
+//  Copyright © 2019 YDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFFFFFFF : UITextField
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ snip/FFFFFFFF.m
@@ -0,0 +1,22 @@
+
+//
+//  FFFFFFFF.m
+//  snip
+//
+//  Created by YDKJ on 2019/11/17.
+//  Copyright © 2019 YDKJ. All rights reserved.
+//
+
+#import "FFFFFFFF.h"
+
+@implementation FFFFFFFF
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
